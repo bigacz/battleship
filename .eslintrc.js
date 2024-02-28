04d8959e58950c5f238a76f42561f720783e7e75
bigacz <sbigacz@gmail.com>
2024-02-28 20:40:57 +0100

    configure jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,10 +14,29 @@ module.exports = {
         sourceType: 'script',
       },
     },
+    {
+      files: ['*.test.js'],
+      env: {
+        jest: true,
+      },
+      plugins: ['jest'],
+      extends: ['plugin:jest/recommended'],
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  rules: {},
+
+  rules: {
+    'no-use-before-define': 'off',
+    'no-unused-vars': 'off',
+    'consistent-return': 'off',
+    'no-console': 'off',
+    'max-classes-per-file': 'off',
+    'import/extensions': 'off',
+    'prefer-const': 'off',
+    'no-empty': 'off',
+    'no-param-reassign': 'off',
+  },
 };

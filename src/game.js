5d26a4f7c9b028a9271f0e32dba0b6a32dc32932
bigacz <sbigacz@gmail.com>
2024-03-27 00:09:31 +0100

    refactor game barrel

--- src/game.js
+++ src/game.js
@@ -1,92 +1,37 @@
 import PubSub from 'pubsub-js';
-import BoardUi from './components/BoardUi';
-import Player from './scripts/Player';
+import GameElement from './scripts/GameElement';
 import endScreen from './components/endScreen';
-
-// Temporary
-
-const player0 = new Player('Anna', false);
-const player1 = new Player('John', true);
-
-//
 
 const parent1 = document.getElementById('board1');
 const parent2 = document.getElementById('board2');
 
-const boardUi0 = new BoardUi(parent1, 0);
-const boardUi1 = new BoardUi(parent2, 1);
-
-const boardUis = [boardUi0, boardUi1];
-const players = [player0, player1];
+const gameElement0 = new GameElement('Player 1', false, parent1, 0);
+const gameElement1 = new GameElement('Player 2', true, parent2, 1);
+const gameElements = [gameElement0, gameElement1];
 
 // temporary
 
-player0.placeShip(2, 2, true, 3);
-player1.placeShip(2, 2, true, 3);
-
-boardUi0.placeShip(2, 2, true, 3);
-boardUi1.placeShip(2, 2, true, 3);
+gameElement0.placeShip(2, 2, true, 3);
+gameElement1.placeShip(2, 2, true, 3);
 
 //
 
 let currentId = 0;
 let otherId = 1;
 
-let currentPlayer = players[currentId];
-let otherPlayer = players[otherId];
-
-let currentBoardUi = boardUis[currentId];
-let otherBoardUi = boardUis[otherId];
+let current = gameElements[currentId];
+let other = gameElements[otherId];
 
 function switchElements() {
   otherId = currentId;
   currentId = currentId === 0 ? 1 : 0;
 
-  currentPlayer = players[currentId];
-  otherPlayer = players[otherId];
-
-  currentBoardUi = boardUis[currentId];
-  otherBoardUi = boardUis[otherId];
-}
-
-function attackOther(x, y) {
-  otherBoardUi.hitSquare(x, y);
-  otherPlayer.receiveAttack(x, y);
-}
-
-function randomAttackOther() {
-  const [x, y] = otherPlayer.calculateAttack();
-
-  otherBoardUi.hitSquare(x, y);
-  otherPlayer.receiveAttack(x, y);
-}
-
-function isCurrentSunk() {
-  return currentPlayer.areAllSunk();
-}
-
-function isOtherSunk() {
-  return otherPlayer.areAllSunk();
+  current = gameElements[currentId];
+  other = gameElements[otherId];
 }
 
 function isSomeoneSunk() {
-  return isCurrentSunk() || isOtherSunk();
-}
-
-function replacePlayers([name0, isAi0], [name1, isAi1]) {
-  players[0] = new Player(name0, isAi0);
-  players[1] = new Player(name1, isAi1);
-}
-
-function cleanBoards() {
-  // TODO: Add logic board cleaning
-  boardUis.forEach((board) => {
-    board.cleanBoard();
-  });
-}
-
-function isCurrentAi() {
-  return currentPlayer.isAi;
+  return current.areAllSunk() || other.areAllSunk();
 }
 
 function getCurrentId() {
@@ -97,12 +42,12 @@ function getOtherId() {
   return otherId;
 }
 
-function isCurrentHit(x, y) {
-  return currentPlayer.isHit(x, y);
+function getCurrent() {
+  return current;
 }
 
-function isOtherHit(x, y) {
-  return otherPlayer.isHit(x, y);
+function getOther() {
+  return other;
 }
 
 function enableEndScreen() {
@@ -117,17 +62,14 @@ function disableEndScreen() {
 
 const game = {
   switchElements,
-  attackOther,
-  randomAttackOther,
-  isOtherSunk,
-  replacePlayers,
-  cleanBoards,
-  isCurrentAi,
+
   getCurrentId,
   getOtherId,
   isSomeoneSunk,
-  isCurrentHit,
-  isOtherHit,
+
+  getCurrent,
+  getOther,
+
   enableEndScreen,
   disableEndScreen,
 };
@@ -136,10 +78,13 @@ const game = {
 function getWinner() {
   let winner = null;
 
-  if (isCurrentSunk()) {
-    winner = otherPlayer.name;
-  } else if (isOtherSunk()) {
-    winner = currentPlayer.name;
+  const isCurrentSunk = current.areAllSunk();
+  const isOtherSunk = other.areAllSunk();
+
+  if (isCurrentSunk) {
+    winner = other.getName();
+  } else if (isOtherSunk) {
+    winner = current.getName();
   }
 
   return winner;
--- src/gameLoop.js
+++ src/gameLoop.js
@@ -3,19 +3,24 @@ import game from './game';
 
 PubSub.subscribe('square-clicked', (msg, data) => {
   const { boardId, x, y } = data;
+  const other = game.getOther();
 
   const otherId = game.getOtherId();
-  const isHit = game.isOtherHit(x, y);
+  const isHit = other.isHit(x, y);
 
   if (otherId === boardId && !game.isSomeoneSunk() && !isHit) {
-    game.attackOther(x, y);
+    other.receiveAttack(x, y);
 
+    // should continue round when hit
+    other.isShip(x, y);
     endRound();
   }
 });
 
 function endRound() {
-  if (game.isOtherSunk()) {
+  const other = game.getOther();
+
+  if (other.areAllSunk()) {
     game.enableEndScreen();
   } else {
     game.switchElements();
@@ -24,8 +29,11 @@ function endRound() {
 }
 
 function checkIfPlayerIsAi() {
-  if (game.isCurrentAi()) {
-    game.randomAttackOther();
+  const current = game.getCurrent();
+  const other = game.getOther();
+
+  if (current.isAi()) {
+    other.receiveRandomAttack();
 
     endRound();
   }

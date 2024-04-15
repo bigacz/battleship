5945265c0d356107a4135c51e5362ccaea0ea974
bigacz <sbigacz@gmail.com>
2024-04-15 14:22:14 +0200

    rework Player class inheritance

--- src/scripts/Player.js
+++ src/scripts/Player.js
@@ -1,50 +1,11 @@
 import Gameboard from './Gameboard';
 
-class Player {
+class Player extends Gameboard {
   constructor(name, isAi) {
+    super();
+
     this.name = name;
     this.isAi = isAi;
-    this.board = new Gameboard();
-  }
-
-  placeShip(startX, startY, isAxisX, length) {
-    this.board.placeShip(startX, startY, isAxisX, length);
-  }
-
-  removeShip(startX, startY) {
-    this.board.removeShip(startX, startY);
-  }
-
-  receiveAttack(x, y) {
-    this.board.receiveAttack(x, y);
-  }
-
-  calculateAttack() {
-    return this.board.calculateAttack();
-  }
-
-  areAllSunk() {
-    return this.board.areAllSunk();
-  }
-
-  isHit(x, y) {
-    return this.board.isHit(x, y);
-  }
-
-  isShip(x, y) {
-    return this.board.isShip(x, y);
-  }
-
-  getShipStart(x, y) {
-    return this.board.getShipStart(x, y);
-  }
-
-  isLegalToPlaceShip(x, y, isAxisX, length) {
-    return this.board.isLegalToPlaceShip(x, y, isAxisX, length);
-  }
-
-  getShip(x, y) {
-    return this.board.getShip(x, y);
   }
 }
 

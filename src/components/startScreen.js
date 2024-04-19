c985b7d0068e98e6df132e32a5662cfb9b957622
bigacz <sbigacz@gmail.com>
2024-04-19 16:42:44 +0200

    add mode and player switching gui

--- src/components/startScreen.js
+++ src/components/startScreen.js
@@ -1,7 +1,5 @@
-import PubSub from 'pubsub-js';
-
-const nameInput0 = document.getElementById('startscreen-name-0');
-const nameInput1 = document.getElementById('startscreen-name-1');
+const nameParagraph0 = document.getElementById('startscreen-name-0');
+const nameParagraph1 = document.getElementById('startscreen-name-1');
 
 const modeRadio0 = document.getElementById('startscreen-mode-0');
 const modeRadio1 = document.getElementById('startscreen-mode-1');
@@ -10,11 +8,26 @@ const modeRadios = [modeRadio0, modeRadio1];
 const switchButton = document.getElementById('startscreen-switch');
 const playButton = document.getElementById('startscreen-play');
 
-function getInputNameValues() {
-  const name0 = nameInput0.value;
-  const name1 = nameInput1.value;
+let areComputers = [false, true];
 
-  return { name0, name1 };
+function getNames() {
+  const name0 = nameParagraph0.value;
+  const name1 = nameParagraph1.value;
+
+  return [name0, name1];
+}
+
+function updateNames(isComputer0, isComputer1) {
+  if (!isComputer0 && !isComputer1) {
+    nameParagraph0.textContent = 'Player 1';
+    nameParagraph1.textContent = 'Player 2';
+  } else if (isComputer0) {
+    nameParagraph0.textContent = 'Computer';
+    nameParagraph1.textContent = 'Player';
+  } else if (isComputer1) {
+    nameParagraph0.textContent = 'Player';
+    nameParagraph1.textContent = 'Computer';
+  }
 }
 
 function isSelectedComputer() {
@@ -22,3 +35,35 @@ function isSelectedComputer() {
 
   return mode0;
 }
+
+// Helpers
+
+function switchPlayers() {
+  if (isSelectedComputer()) {
+    areComputers = areComputers.toReversed();
+  }
+
+  updateNames(...areComputers);
+}
+
+function changeMode() {
+  if (isSelectedComputer()) {
+    areComputers = [false, true];
+  } else {
+    areComputers = [false, false];
+  }
+
+  updateNames(...areComputers);
+}
+
+// Events
+
+modeRadios.forEach((radio) => {
+  radio.addEventListener('change', () => {
+    changeMode();
+  });
+});
+
+switchButton.addEventListener('click', () => {
+  switchPlayers();
+});

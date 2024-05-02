f161cb9c5c8c1be8f6e9741a33830aef642a4f12
bigacz <sbigacz@gmail.com>
2024-05-02 23:56:47 +0200

    cleanup index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-import 'normalize.css';
 import './styles/styleBarrel';
-import './gameLoop';
-
-// test
-import game from './game';
-
-game.getCurrent().enableDragging();
-game.getOther().enableDragging();
+import './controllers/gameController';
--- src/styles/styleBarrel.js
+++ src/styles/styleBarrel.js
@@ -1,3 +1,4 @@
+import 'normalize.css';
 import './general.css';
 import './boards.css';
 import './endscreen.css';

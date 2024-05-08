a07ba2875cddd3be02ec5ca176644d74b0f9cf9e
bigacz <sbigacz@gmail.com>
2024-05-08 18:09:22 +0200

    add missing files

--- src/styles/styleBarrel.js
+++ src/styles/styleBarrel.js
@@ -1,6 +1,12 @@
 import 'normalize.css';
 import './general.css';
+import './fonts.css';
+import './variables.css';
+
+import './header.css';
 import './boards.css';
 import './endscreen.css';
 import './startScreen.css';
 import './switchScreen.css';
+import './controlPanel.css';
+import './messageBoard.css';
